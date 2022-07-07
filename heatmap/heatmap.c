7aa169141a6777cdf95d54914b8b3f373ae15e0e
bilalfatian <92918987+bilalfatian@users.noreply.github.com>
2022-07-07 22:17:42 +0000

    Update heatmap.c

--- heatmap/heatmap.c
+++ heatmap/heatmap.c
@@ -1,4 +1,37 @@
-#include "heatmap.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct map{
+    unsigned dim;
+    char** head;
+};
+
+struct heatmap
+{    
+    unsigned dim;
+    int** head;
+};
+
+
+struct piece{
+    unsigned row;
+    unsigned col;
+    char** head;
+};
+
+typedef struct map map;
+typedef struct heatmap heatmap;
+typedef struct piece piece;
+
+map createMap(unsigned);    
+void showMap(map);
+int encercle(heatmap, map);
+heatmap makeheatmap(map);
+
+
+
 
 int max = 0;
 
